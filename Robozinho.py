8f8049d62a7fae7ea2a6be364d16f8f015f6a238
kevin <kevinmarques123>
2021-09-22 16:14:18 -0300

    menos um s

--- Robozinho.py
+++ Robozinho.py
@@ -6,7 +6,7 @@ jogo = {'jogador1': randint(1, 6),
         'jogador3': randint(1, 6),
         'jogador4': randint(1, 6)}
 ranking = dict()
-print('valores sorteadoss:')
+print('valores sorteados:')
 for k, v in jogo.items():
     print(f'{k} tirou {v} no dado.')
     sleep(1)
